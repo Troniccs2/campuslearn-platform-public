65550006a381904c39a67d52c60b710b68f4c45f
JavaPanda577656 <141138713+JavaPanda577656@users.noreply.github.com>
2025-10-09 13:27:44 +0200

    Delete CampusLearn directory

--- CampusLearn/CampusLearn/src/main/java/com/thesensationals/campuslearn/repository/PrivateMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thesensationals.campuslearn.repository;
-
-import com.thesensationals.campuslearn.models.PrivateMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrivateMessageRepository extends JpaRepository<PrivateMessage, Long> {
-}
--- CampusLearn/CampusLearn/src/main/java/com/thesensationals/campuslearn/repository/TutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thesensationals.campuslearn.repository;
-
-import com.thesensationals.campuslearn.models.Tutor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TutorRepository extends JpaRepository<Tutor, String> {
-}
